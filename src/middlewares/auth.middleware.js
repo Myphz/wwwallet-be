20fd796dde10ab8cf2081cbe9ecea56fbaa7f4a9
Myphz <danielscanu45@outlook.it>
2022-02-28 14:42:53 +0100

    Added auth middleware with JWT strategy

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const User = require("mongoose").model("User");
+const { JWT_KEY } = require("../config/config");
+
+// Retrieves the JWT token from cookies
+const jwtFromRequest = req => {
+  return req?.cookies?.jwt || null; 
+};
+
+// Options for the strategy
+const opts = { 
+  jwtFromRequest,
+  secretOrKey: JWT_KEY 
+};
+
+// Check if the user exists, given its id
+const authStratregy = new JwtStrategy(opts, (jwtToken, cb) => {
+User.findById(jwtToken.sub, (err, user) => {
+    // Error + no user
+    if (err) return cb(err, false);
+    // No error + user
+    if (user) return cb(null, user);
+    // No error + no user
+    return cb(null, false);
+  });
+});
+
+passport.use(authStratregy);
+module.exports = passport.authenticate('jwt', { session: false });
