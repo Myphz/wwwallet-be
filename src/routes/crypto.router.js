78c69bf1c00e8329903d49773d8e98f5106e70df
Myphz <danielscanu45@outlook.it>
2022-04-22 12:06:08 +0200

    fix: rename endpoint from `/*` to `/binance/*`

--- src/routes/crypto.router.js
+++ src/routes/crypto.router.js
@@ -6,7 +6,7 @@ import { BINANCE_ERROR } from "../config/errors.js";
 const router = express.Router();
 
 // Middleware endpoint to redirect binance API calls (as they don't work in the browser)
-router.get("/*", async (req, res, next) => {
+router.get("/binance/*", async (req, res, next) => {
   try {
     // Construct the URL with the given endpoint and params
     const data = await fetch(`${BINANCE_BASE_URL}${req.params[0]}?${new URLSearchParams(req.query)}`);
