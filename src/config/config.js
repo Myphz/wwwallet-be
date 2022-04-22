13ff6ac79e312b6e9835a0935f5abed826c7b277
Myphz <danielscanu45@outlook.it>
2022-04-22 11:10:41 +0200

    test: add env variable NODE_ENV to set mongo uri

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ config();
 
 export const PORT = 3000;
 export const JWT_KEY = process.env.JWT_KEY;
-export const MONGO_URI = process.env.MONGO_TEST_URI || process.env.MONGO_URI;
+export const MONGO_URI = process.env.NODE_ENV === "test" ? process.env.MONGO_TEST_URI : process.env.MONGO_URI;
 export const SALT_ROUNDS = 10;
 export const COOKIE_OPTS = {
   secure: true,
