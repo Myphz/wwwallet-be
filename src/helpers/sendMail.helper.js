583eaabb8ec76bec939c97f0fc0ab0d30a26acc4
Myphz <danielscanu45@outlook.it>
2022-05-02 21:19:55 +0200

    feature: add default EMAIL.contact param to renderTemplate, template improvements

--- src/helpers/sendMail.helper.js
+++ src/helpers/sendMail.helper.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer"
 import { promises as fs } from "fs";
-import { EMAIL_SETTINGS, TEMPLATES_PATH } from "../config/config.js";
+import { EMAIL, EMAIL_SETTINGS, TEMPLATES_PATH } from "../config/config.js";
 
 export default async function sendEmail(template, to, from, subject, params) {
   const transport = nodemailer.createTransport(EMAIL_SETTINGS);
@@ -18,12 +18,15 @@ export default async function sendEmail(template, to, from, subject, params) {
   };
 
   options.html = await renderTemplate(template, params);
+
   await transport.sendMail(options);
 };
 
+// Parse HTML file with the requested parameters
 async function renderTemplate(template, params) {
   let file = await fs.readFile(`${TEMPLATES_PATH}${template}.html`, { encoding: "utf-8" });
-  for (const [key, value] of Object.entries(params)) {
+  // Replace every {{ key }} in the HTML file with the corresponding value
+  for (const [key, value] of Object.entries({ contactEmail: EMAIL.contact, ...params })) {
     const regex = new RegExp(`{{ ${key} }}`, "g");
     file = file.replace(regex, value);
   }
