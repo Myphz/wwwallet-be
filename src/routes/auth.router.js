14e9bc15ff5cb4406e51785e1556e8412f460070
Myphz <danielscanu45@outlook.it>
2022-05-05 16:04:04 +0200

    style: convert tabs to spaces

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -26,12 +26,12 @@ const validator = {
 // This method can be called with the request parameter "resend" to just resend the email, without
 // creating a new user.
 router.post("/register", validateParams(validator), (req, res, next) => {
-	const { email, password, resend } = req.body;
+  const { email, password, resend } = req.body;
   if (resend) {
     // Don't create a new user, just verify that the user exists, create the jwt token and send the email
     return User.findOne({ email, isVerified: false }, { _id: 1 }, (err, user) => {
       if (err || !user) return next(CREDENTIALS_ERROR);
-		  const jwt = issueJWT(user);
+      const jwt = issueJWT(user);
       sendMail("confirmEmail", email, EMAIL.noreply, "Confirm your email address", { codeLink: `${BASE_URL}?jwt=${jwt}` });
       // Return success
       return res.json({ success: true });
@@ -39,16 +39,16 @@ router.post("/register", validateParams(validator), (req, res, next) => {
   };
   
   // Create new user and try to save it
-	const user = new User({ email, password });
-	user.save((err, user) => {
+  const user = new User({ email, password });
+  user.save((err, user) => {
     // If an error has occurred, the email is already registered
-		if (err) return next(EMAIL_REGISTERED_ERROR);
+    if (err) return next(EMAIL_REGISTERED_ERROR);
     // Save the jwt token and send verification email with the jwt token
-		const jwt = issueJWT(user);
+    const jwt = issueJWT(user);
     sendMail("confirmEmail", email, EMAIL.noreply, "Confirm your email address", { codeLink: `${BASE_URL}?jwt=${jwt}` });
     // Return success
-		res.json({ success: true });
-	});
+    res.json({ success: true });
+  });
 });
 
 // Confirm email endpoint
@@ -68,39 +68,39 @@ router.post("/register/verify", validateParams({ jwt: { type: String } }), (req,
 // Login endpoint
 router.post("/login", validateParams(validator, CREDENTIALS_ERROR), (req, res, next) => {
   // If any field is missing, return error
-	const { email, password } = req.body;
-	User.checkLogin(email, password)
+  const { email, password } = req.body;
+  User.checkLogin(email, password)
     // If the email has been found, check if the password match with the login parameter
-		.then(({ login, user }) => {
-			if (!login) return next(CREDENTIALS_ERROR);
+    .then(({ login, user }) => {
+      if (!login) return next(CREDENTIALS_ERROR);
       // If the password match and the user is verified, issue the jwt token
-			if (user.isVerified) res.cookie("jwt", issueJWT(user), COOKIE_OPTS);
+      if (user.isVerified) res.cookie("jwt", issueJWT(user), COOKIE_OPTS);
       // Return success and isVerified property
-			res.json({ success: true, isVerified: user.isVerified });
-		})
+      res.json({ success: true, isVerified: user.isVerified });
+    })
     // If an error occured, the email isn't registered
-		.catch(_ => {
-			return next(CREDENTIALS_ERROR);
-		});
+    .catch(_ => {
+      return next(CREDENTIALS_ERROR);
+    });
 });
 
 // Logout the user (replace the jwt cookie and return success)
 router.delete("/login", (req, res) => {
-	res.cookie("jwt", "", COOKIE_OPTS);
-	res.json({ success: true });
+  res.cookie("jwt", "", COOKIE_OPTS);
+  res.json({ success: true });
 });
 
 // Verify a user (already using the middleware, so just return success)
 router.get("/verify", authMiddleware, (req, res) => {
-	res.json({ success: true });
+  res.json({ success: true });
 });
 
 // Error handler function
 router.use((err, req, res, next) => {
   // Set empty jwt token as cookie
   res.cookie("jwt", "", COOKIE_OPTS);
-	res.status(err.status);
-	res.json({ success: false, msg: err.message });
+  res.status(err.status);
+  res.json({ success: false, msg: err.message });
 });
 
 export default router;
