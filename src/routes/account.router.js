c42241a5a24d68101c908f672104fdad11a30808
Myphz <danielscanu45@outlook.it>
2022-05-05 18:19:59 +0200

    feature: DELETE /api/account/delete endpoint

--- src/routes/account.router.js
+++ src/routes/account.router.js
@@ -1,10 +1,12 @@
 import express from "express";
+import User from "../models/user.js";
 import validateParams from "../middlewares/validateParams.middleware.js";
 import { BASE_URL, COOKIE_OPTS, EMAIL } from "../config/config.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 import { validateEmail, validatePassword } from "../helpers/validateParams.helper.js";
 import { decodeJWT, issueJWT } from "../helpers/jwt.helper.js";
 import sendMail from "../helpers/sendMail.helper.js";
+import { CREDENTIALS_ERROR, EXPIRED_LINK } from "../config/errors.js";
 
 const router = express.Router();
 
@@ -34,15 +36,20 @@ router.post("/delete", authMiddleware, (req, res, next) => {
 router.delete("/delete", validateParams({ jwt: { type: String } }, { location: "query" }), (req, res, next) => {
   const jwt = decodeJWT(req.query.jwt);
   // Throw error if the jwt is invalid
-  if (!jwt) return next(CREDENTIALS_ERROR);
+  if (!jwt || !jwt.delete) return next(EXPIRED_LINK);
+  User.deleteOne({ _id: jwt.sub, isVerified: true }, err => {
+    if (err) return next(CREDENTIALS_ERROR);
+    res.cookie("jwt", "", COOKIE_OPTS);
+    res.json({ success: true });
+  });
 });
 
 // Error handler function
-// router.use((err, req, res, next) => {
-//   // Set empty jwt token as cookie
-//   res.cookie("jwt", "", COOKIE_OPTS);
-//   res.status(err.status);
-//   res.json({ success: false, msg: err.message });
-// });
+router.use((err, req, res, next) => {
+  // Set empty jwt token as cookie
+  res.cookie("jwt", "", COOKIE_OPTS);
+  res.status(err.status);
+  res.json({ success: false, msg: err.message });
+});
 
 export default router;
