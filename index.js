13846eed0165d0e04e521e2ac885321c8aa21e82
Myphz <danielscanu45@outlook.it>
2022-05-09 22:36:10 +0200

    fix: fix serve static middleware and api router path

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+import express from "express";
 import app from "./src/config/app.js";
 import { PORT } from "./src/config/config.js";
 import apiRouter from "./src/routers/api.router.js";
 import { logError } from "./src/helpers/logger.helper.js";
 import history from "connect-history-api-fallback";
 
+app.use("/api", apiRouter);
+
 const staticMiddleware = express.static("dist");
-
 app.use(staticMiddleware);
-
 // Support history api
 // this is the HTTP request path not the path on disk
 app.use(history({ index: "/index.html" }));
-
 // 2nd call for redirected requests
 app.use(staticMiddleware);
 
-app.use("/api", apiRouter);
-
+// Global error handler function
 app.use((err, req, res, next) => {
+  // Check if the error object has a status (i.e, if it's one from config/errors.js)
   const isExpected = !!err.status;
+  // Set the status if declared or 500
   res.status(err.status || 500);
+  // Send the error message if expected
 	res.json({ success: false, msg: isExpected ? err.message : "Server Error" });
+  // Log error if not expected
   if (!isExpected) logError(err);
 });
 
--- src/routers/api.router.js
+++ src/routers/api.router.js
@@ -2,13 +2,13 @@ import express from "express";
 import authRouter from "./auth.router.js";
 import cryptoRouter from "./crypto.router.js";
 import transactionsRouter from "./transactions.router.js";
-import accountRouter from "./routers/account.router.js";
+import accountRouter from "./account.router.js";
 
 const router = express.Router();
 
-router.use("/api/auth", authRouter);
-router.use("/api/crypto", cryptoRouter);
-router.use("/api/transactions", transactionsRouter);
-router.use("/api/account", accountRouter);
+router.use("/auth", authRouter);
+router.use("/crypto", cryptoRouter);
+router.use("/transactions", transactionsRouter);
+router.use("/account", accountRouter);
 
 export default router;
