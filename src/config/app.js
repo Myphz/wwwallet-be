32f30d62d9a9e389d3036eed4dbacd08db693b90
Myphz <danielscanu45@outlook.it>
2022-05-19 15:27:24 +0200

    fix: add `localhost` to CORS

--- src/config/app.js
+++ src/config/app.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 import { BASE_URL } from "./config.js";
 
 const app = express();
-app.use(cors({ origin: BASE_URL, credentials: true }));
+app.use(cors({ origin: [BASE_URL, "http://localhost"], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
