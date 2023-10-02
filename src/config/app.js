695c7b15a32c47bcaca627011579d009233a86de
Myphz <danielkeq@gmail.com>
2023-10-02 23:05:10 +0200

    fix: location

--- src/config/app.js
+++ src/config/app.js
@@ -16,8 +16,8 @@ app.use(cookieParser());
 
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // For express-rate-limit
-if (process.env.NODE_ENV !== "production") {
-  app.set("trust proxy", 2);
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.set("trust proxy", 2);
+// }
 
 export default app;
